7523f838d8d2eaceef23a4484e66c319d192b732
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2024-01-10 12:22:22 -0300

    finalizado os teste de login

--- cypress/e2e/UI/conexaoQA_login.cy.js
+++ cypress/e2e/UI/conexaoQA_login.cy.js
@@ -1,5 +1,9 @@
 describe.only('página de login', () => {
     
+    const CAMPO_EMAIL = 'login-email'
+    const CAMPO_SENHA = 'login-password'
+    const CAMPO_LOGIN = 'login-submit'
+
     beforeEach(() => {
         cy.visit('/login')
     })
@@ -9,15 +13,15 @@ describe.only('página de login', () => {
         cy.intercept('GET', '/api/profile/me')
             .as('apiLogin')
 
-        cy.getElement('login-email')
+        cy.getElement(CAMPO_EMAIL)
             .type(Cypress.env('email'))
 
             // preenche a senha
-            cy.getElement('login-password')
+            cy.getElement(CAMPO_SENHA)
                 .type(Cypress.env('senha'))
 
             // clica no login
-            cy.getElement('login-submit')
+            cy.getElement(CAMPO_LOGIN)
                 .click()
             
             // espera a api responder    
@@ -30,5 +34,51 @@ describe.only('página de login', () => {
             cy.getElement('dashboard-welcome')
                 .should('be.visible')
                 .and('contain', 'HUMBERTO JOSE DANTAS')
+    })
+
+    it('faz um login inválido', () => {
+        
+        // spy na api de login
+        cy.intercept('POST', '/api/auth')
+            .as('apiLogin')
+
+        // preencher o email
+        cy.getElement(CAMPO_EMAIL)
+            .type(Cypress.env('email'))
+
+        // preencher a senha
+        cy.getElement(CAMPO_SENHA)
+                .type('abcdef')
+
+        // clicar no botao login
+        cy.getElement(CAMPO_LOGIN)
+                .click()
+
+        // esperar a API
+        cy.wait('@apiLogin')
+            .then(({ response }) => {
+                expect(response.statusCode, 'Validação do HTTP Status Code').to.eq(401)
+            })
+
+        // validar a mensagem de retorno (erro)
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+    })
+
+    it('valida a digitação de um email inválido', () => {
+        
+        // preencher o email (inválido)
+        cy.getElement(CAMPO_EMAIL)
+            .type('Humberto')
+
+        // preencher a senha
+        cy.getElement(CAMPO_SENHA)
+                .type('abcdef')    
+        
+        // validar a máscara do campo
+        cy.contains('p', 'Digite um email válido')
+            .should('be.visible')
+        
+            
     })
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,5 @@
+require('cypress-plugin-tab')
+
 // ***********************************************************
 // This example support/e2e.js is processed and
 // loaded automatically before your test files.
