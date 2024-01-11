d964a7f7600db936826d3cd1d8ef01fc41f5e9aa
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2024-01-11 12:11:27 -0300

    test case de alerta finalizado

---
+++ cypress/e2e/UI/conexaoQA_alerta.cy.js
@@ -0,0 +1,47 @@
+describe('alerta de login', () => {
+    
+    it('valida o alerta de credencial inválida', () => {
+
+        // faz o spy na hora
+        cy.clock()
+
+        // intercepta a API de Login
+        cy.intercept('POST', '/api/auth')
+            .as('login')
+
+        // visita a página de login
+        cy.visit('/login')
+
+        // preencher um email aleatorio
+        cy.getElement('login-email')
+            .type('usuarioALeatorio@teste.com')
+
+        // preencher uma senha aleatoria
+        cy.getElement('login-password')
+            .type('123456')
+
+        // clicar no botao Login
+        cy.getElement('login-submit')
+            .click()
+        
+        cy.wait('@login')
+
+        // Validar o alerta de credencial inválido
+        cy.getElement('alert')
+            .should('have.text', 'Credenciais inválidas')
+
+        // cy.wait(10000)
+        
+        // cy.getElement('alert') função customizada não permite options
+        // cy.get('[data-test=alert]', { timeout:10000 })
+        //    .should('not.exist')
+
+        // adiantar o tempo da aplicacao em 10 segundos
+        cy.tick(10000)
+        
+        cy.getElement('alert')
+            .should('not.exist')
+
+    })
+
+})
